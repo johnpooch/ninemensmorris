aa0ef987e68bb6e51e7042a59ee4b084260cd818
John McDowell <johnmcdowell0801@gmail.com>
2018-05-25 10:37:51 +0000

    turns not working

--- game.py
+++ game.py
@@ -1,6 +1,14 @@
+import os
+
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+
+
+
 
 validPositionsList = [[0,3,6],[1,3,5],[2,3,4],[0,1,2,4,5,6],[2,3,4],[1,3,5],[0,3,6]]
-# nullColumnsList = [[2,3,5,6],[1,3,5,7],[1,2,6,7],[4],[1,2,6,7],[1,3,5,7],[2,3,5,6]]
 
 playerColorDict = {1: "white", 2: "black"}
 
@@ -33,65 +41,85 @@ neighboursDictionary = {
     (6,6) : [(6,3), (3,6)]
 }
 
-# Use c profiler python3 -m cProfile boggle.py
+# Use c profiler to check speed: python3 -m cProfile boggle.py 
+# Build testing
 
+
+
+
+
+# def getCoordinatesFromUser(currentPlayer):
+
+#     y = 
+#     x = 
+#     return (x, y)
+    
+# def threeInARow(gameBoardDictionary):
+#     for position in middlePositions:
+#         if(checkNeighboursAreSameColor(position)):
+#             return True
+            
+
+# def checkNeighboursAreSameColor(position):
+#     for i in neighboursDictionary[position]:
+#         for j in neighboursDictionary[position]:
+#             if (gameBoardDictionary[i] == gameBoardDictionary[j]) and (i != j) and (gameBoardDictionary[i] != "empty") and ((i[0] == j[0]) or (i[1] == j[1])):
+#                 return True
+#     return False
+
+# def removeOpponentPiece(currentPlayer):
+#     positionToRemove = getCoordinatesFromUser(currentPlayer)
+#     if (gameBoardDictionary[(positionToRemove)] != "empty" and gameBoardDictionary[(positionToRemove)] != playerColorDict[currentPlayer]): 
+#         gameBoardDictionary[(positionToRemove)] = "empty"
+#         return gameBoardDictionary
+        
+#     else:
+#         print ("Invalid move")
+#         removeOpponentPiece(currentPlayer)
+        
+# def placePiecesPhase(gameBoardDictionary):
+#     for i in range(9):
+#         gameBoardDictionary = userTurn(1, gameBoardDictionary)
+#         gameBoardDictionary = userTurn(2, gameBoardDictionary)
+#     print("all pieces placed")
+#     return gameBoardDictionary
+
+# def play(gameBoardDictionary):
+    
+#     gameBoardDictionary = placePiecesPhase(gameBoardDictionary);
+    
 gameBoardDictionary = {}
 
 def createGameBoard():
     gameBoardDictionary = {(i, j) : "empty" for i in range(7) for j in range(7) for k in validPositionsList[i] if j == k}
     return gameBoardDictionary
-
-def userTurn(currentPlayer, gameBoardDictionary):
     
-    positionToPlace = getCoordinatesFromUser(currentPlayer)
-    
-    if (gameBoardDictionary[positionToPlace] == "empty"): 
-        
-        gameBoardDictionary[(positionToPlace)] = playerColorDict[currentPlayer]
-        if(threeInARow(gameBoardDictionary)):
-            print("Three in a Row!")
-            gameBoardDictionary = removeOpponentPiece(currentPlayer)
-            
-        print(gameBoardDictionary)
+def placePiece(position, currentPlayer):
+    if (gameBoardDictionary[position] == "empty"): 
+        gameBoardDictionary[(position)] = currentPlayer
         return gameBoardDictionary
-        
     else:
         print ("Invalid move")
-        userTurn(currentPlayer, gameBoardDictionary)
-
-def getCoordinatesFromUser(currentPlayer):
-
-    y = int(input("P{} enter y-coordinate of piece: ".format(currentPlayer)))
-    x = int(input("P{} enter x-coordinate of piece: ".format(currentPlayer)))
-    return (x, y)
-    
-def threeInARow(gameBoardDictionary):
-    for position in middlePositions:
-        if(checkNeighboursAreSameColor(position)):
-            return True
-            
-
-def checkNeighboursAreSameColor(position):
-    for i in neighboursDictionary[position]:
-        for j in neighboursDictionary[position]:
-            if (gameBoardDictionary[i] == gameBoardDictionary[j]) and (i != j) and (gameBoardDictionary[i] != "empty") and ((i[0] == j[0]) or (i[1] == j[1])):
-                return True
-    return False
-
-def removeOpponentPiece(currentPlayer):
-    positionToRemove = getCoordinatesFromUser(currentPlayer)
-    if (gameBoardDictionary[(positionToRemove)] != "empty" and gameBoardDictionary[(positionToRemove)] != playerColorDict[currentPlayer]): 
-        gameBoardDictionary[(positionToRemove)] = "empty"
-        return gameBoardDictionary
-        
-    else:
-        print ("Invalid move")
-        removeOpponentPiece(currentPlayer)
-
-def play(gameBoardDictionary):
-    userTurn(1, gameBoardDictionary)
-    userTurn(2, gameBoardDictionary)
     
 gameBoardDictionary = createGameBoard()
-while(True):
-    play(gameBoardDictionary)
+
+@app.route("/")
+def get_index():
+    return render_template("index.html", gameBoardDicitonary=gameBoardDictionary)
+    
+@app.route("/accept_user_input/<player>", methods=['GET', 'POST'])
+def accept_user_input(player):
+    
+    position = tuple(map(int, request.form['position'].split(' ')))
+    gameBoardDictionary = placePiece(position, player)
+    
+    if player == "white":
+        player = "black"
+    else:
+        player = "white"
+        
+    print(player)
+        
+    return render_template("index.html", gameBoardDicitonary=gameBoardDictionary)
+
+app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
